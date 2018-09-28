62cc1754a1d549491cb6d84f27504ce47c337f6f
hannakebebew <hannakebebew@10.0.0.202>
2018-09-28 08:59:15 -0400

    change on work frame

--- SeleniumProject/src/test/java/PageObjectModel/LinkedinElements.java
+++ SeleniumProject/src/test/java/PageObjectModel/LinkedinElements.java
@@ -7,6 +7,7 @@ public class LinkedinElements {
 	
 	WebDriver driver; 
 	
+	
 	public void LinkedinLogin (String name, String pass) {
 		driver.findElement(By.id("session_key-login")).sendKeys(name);
 		driver.findElement(By.id("session_password-login")).sendKeys(pass);
